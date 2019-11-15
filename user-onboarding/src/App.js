43fdbc536a41674fbad92ed4749af41d1f0c026c
Walter Futch <walter.futch@gmail.com>
2019-11-14 19:03:49 -0500

    MVP Complete
    MVP Complete

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FormikUserOnboard from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <FormikUserOnboard />
+    </div>
+  );
+}
+
+export default App;
